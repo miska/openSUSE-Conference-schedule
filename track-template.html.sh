4855f0d1febb43e907b884ceb7cd7f85a0f94911
Michal Hrusecky <Michal@Hrusecky.net>
2012-09-06 22:48:22 +0200

    Little formating change

--- track-template.html.sh
+++ track-template.html.sh
@@ -2,10 +2,11 @@
 echo "<div class='modal fade' id='$code'>
   <div class='modal-header'>
     <a class='close' data-dismiss='modal'>&times;</a>
-    <h3><em>$authors</em>:<br/>$title</h3>
+    <h3>$title</h3>
   </div>
   <div class='modal-body'>
     <img style='float: right;' src='static/$skills-big.png'/>
+    <p><strong>Speaker(s):</strong> <em>$authors</em></p>
     <p><strong>Type:</strong> $type (`echo "$skills" | sed -e 's|B|Beginners|' -e 's|I|Skilled users|' -e 's|H|Hardcore|'`)</p>
     <p><strong>Language:</strong> `echo $lang | sed -e 's|EN|English|' -e 's|CZ|Czech|'` <img src='static/$lang.png'/></p>
     <p><strong>When:</strong> `echo "$time" | sed 's|-|\ -\ |'` `echo "$day" | sed -e 's|Sat|Saturday|' -e 's|Sun|Sunday|'`</p>
